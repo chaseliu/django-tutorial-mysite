5b920b1feb7bed4e917974f5c4e55b5224983944
Chi Liu <ichase.liu@gmail.com>
2018-05-10 23:41:11 +0800

    Finish tutorial

---
+++ polls/urls.py
@@ -0,0 +1,24 @@
+"""Poll app routes"""
+
+from django.urls import path, include
+
+from . import views
+
+app_name = 'polls'  # URL namespace
+
+# view functions
+normal_views = [
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+
+generic_views = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+
+urlpatterns = generic_views
